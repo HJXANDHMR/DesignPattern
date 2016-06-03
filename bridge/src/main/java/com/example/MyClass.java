c9698b8cd66f975990ec25aa328a474c2f364f68
hejianxiong <975464817@qq.com>
2016-06-03 15:04:08 +0800

    add images

--- bridge/src/main/java/com/example/MyClass.java
+++ bridge/src/main/java/com/example/MyClass.java
@@ -4,6 +4,7 @@ public class MyClass {
 
     public static void main(String[] args) {
         ITV itv = new LeTV();
+        itv.on();
         LogitechRemoteControl logitechRemoteControl = new LogitechRemoteControl(itv);
         logitechRemoteControl.setChannelKeyboard(30);
     }
