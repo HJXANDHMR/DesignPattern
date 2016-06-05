ab3063022521f7a2d016e2d20f88324a73638d66
hejianxiong <975464817@qq.com>
2016-06-05 22:08:24 +0800

    modify

--- proxy/src/main/java/com/example/MyClass.java
+++ proxy/src/main/java/com/example/MyClass.java
@@ -3,9 +3,7 @@ package com.example;
 public class MyClass {
 
     public static void main(String[] args) {
-
         AbstractObject abstractObject = new ProxyObject();
-
         abstractObject.operation();
     }
 }
