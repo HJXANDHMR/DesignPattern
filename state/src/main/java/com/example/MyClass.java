7c3a82eb3d8006ce2fa760d11b8d0f11f1df80ff
hejianxiong <975464817@qq.com>
2016-06-11 15:33:31 +0800

    modify code

--- state/src/main/java/com/example/MyClass.java
+++ state/src/main/java/com/example/MyClass.java
@@ -3,9 +3,8 @@ package com.example;
 public class MyClass {
 
     public static void main(String[] args) {
-        WaterContext waterContext = new WaterContext();
         IWaterState iWaterState;
-
+        WaterContext waterContext = new WaterContext();
         //模拟状态改变
         for (int i = 0; i < 3; i++) {
             waterContext.setIWaterState(i);
