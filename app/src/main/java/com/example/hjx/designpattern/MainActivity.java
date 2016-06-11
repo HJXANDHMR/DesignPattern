8d8993556f823429dc9a4195fd361a33e097d7fe
hejianxiong <975464817@qq.com>
2016-06-11 15:01:41 +0800

    modify

--- app/src/main/java/com/example/hjx/designpattern/MainActivity.java
+++ app/src/main/java/com/example/hjx/designpattern/MainActivity.java
@@ -1,31 +1,15 @@
 package com.example.hjx.designpattern;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Handler mHandler = new Handler() {
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Message message = Message.obtain();
-        mHandler.sendMessage(message);
-
-
     }
 
 }
